b679bbac6f25bcf6d5ebcb1293280c745e011735
junhui to <junhuito5771@gmail.com>
2023-03-15 18:22:21 +0800

    chore!:Convert configuration DB_PORT to number

--- src/configuration.ts
+++ src/configuration.ts
@@ -26,7 +26,7 @@ export const TYPE_ORM_MODULE_ROOT: TypeOrmModule = TypeOrmModule.forRootAsync({
   useFactory: (config: ConfigService): TypeOrmModuleOptions => ({
     type: 'postgres',
     host: config.get<string>('DB_HOST'),
-    port: config.get<number>('DB_PORT'),
+    port: Number(config.get<number>('DB_PORT')),
     username: config.get<string>('DB_USERNAME'),
     password: config.get<string>('DB_PASSWORD'),
     database: config.get<string>('DB_NAME'),
